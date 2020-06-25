8532403cd661f2380eb6fb3f29fcc5d94859ee62
SURYAJITH NAIR <suryajith082@gmail.com>
2020-06-25 23:26:56 +0530

    Added thread for better working of ocr in wpf

--- wpf image to text tesseract ocr application/MainWindow.xaml.cs
+++ wpf image to text tesseract ocr application/MainWindow.xaml.cs
@@ -18,6 +18,7 @@ using System.Windows.Navigation;
 using System.Drawing.Imaging;
 using System.Drawing.Drawing2D;
 using System.IO;
+using System.Threading;
 
 namespace wpf_image_to_text_tesseract_ocr_application
 {
@@ -31,31 +32,43 @@ namespace wpf_image_to_text_tesseract_ocr_application
             InitializeComponent();
         }
 
-        private void StartButton_Click(object sender, RoutedEventArgs e)
+        public void Compile()
         {
             //Creating a Rectangle object which will  
             //capture our Current Screen
             Rectangle captureRectangle = Screen.AllScreens[0].Bounds;
             //Creating a new Bitmap object
+            textBox.Dispatcher.Invoke(new Action(() => textBox.Text = "created rectangle"));
             Bitmap captureBitmap = new Bitmap(captureRectangle.Right - captureRectangle.Left, captureRectangle.Bottom - captureRectangle.Top, PixelFormat.Format32bppArgb);
             //Bitmap captureBitmap = new Bitmap(int width, int height, PixelFormat);
             //Creating a New Graphics Object
+            textBox.Dispatcher.Invoke(new Action(() => textBox.Text = "created bitmap"));
             Graphics captureGraphics = Graphics.FromImage(captureBitmap);
             //Copying Image from The Screen
+            textBox.Dispatcher.Invoke(new Action(() => textBox.Text = "creating graphics"));
             captureGraphics.CopyFromScreen(captureRectangle.Left, captureRectangle.Top, 0, 0, captureRectangle.Size);
             //Saving the Image File
-
+            textBox.Dispatcher.Invoke(new Action(() => textBox.Text = "copied from screen"));
             //captureBitmap.Save(@"Capture.jpg", ImageFormat.Jpeg);
             using (var image = new Image<Bgr, byte>(captureBitmap))
             {
+                textBox.Dispatcher.Invoke(new Action(() => textBox.Text = "started process"));
                 using (var tess = new Tesseract("C:\\Users\\Acer PC\\source\\repos\\test and try project\\test and try project\\obj\\Debug\\tessdata", "eng", OcrEngineMode.TesseractCubeCombined))
                 {
                     tess.Recognize(image);
+                    textBox.Dispatcher.Invoke(new Action(() => textBox.Text = "recognizing"));
                     string text = tess.GetText().TrimEnd();
                     //Console.WriteLine(text);
-                    textBox.Text = text;
+                    textBox.Dispatcher.Invoke(new Action(()=>textBox.Text=text));
                 }
             }
+        }
+
+        private void StartButton_Click(object sender, RoutedEventArgs e)
+        {
+            //Creating a new thread which will do background process of a button click event in another thread
+            Thread thread = new Thread(Compile);
+            thread.Start();
         }
 
         private void StopButton_Click(object sender, RoutedEventArgs e)
